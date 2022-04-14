47a6698ddcf7e25f7aeb99a926ba8db0c300b191
jmbuck4 <jmbuck4@ncsu.edu>
2022-04-14 12:23:03 -0400

    fix billtest import

--- iTrust2/src/test/java/edu/ncsu/csc/iTrust2/api/APIBillControllerTest.java
+++ iTrust2/src/test/java/edu/ncsu/csc/iTrust2/api/APIBillControllerTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
