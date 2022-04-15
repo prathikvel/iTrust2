01d71739ff268aba655b282431802126bfd44d5c
clscanlo <clscanlo@ncsu.edu>
2022-04-15 17:21:24 -0400

    repo  fix

--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/repositories/PaymentRepository.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/repositories/PaymentRepository.java
@@ -2,24 +2,14 @@ package edu.ncsu.csc.iTrust2.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import edu.ncsu.csc.iTrust2.models.CPTCode;
 import edu.ncsu.csc.iTrust2.models.Payment;
-import edu.ncsu.csc.iTrust2.models.User;
 
 /**
+ * Payment repo
  *
  * @author colinscanlon
  *
  */
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
-    /**
-     * Finds an ICDCode by the provided code
-     *
-     * @param patient
-     *            Code to search by
-     * @return Matching code, if any
-     */
-    public CPTCode findByPatient ( User patient );
 
 }
